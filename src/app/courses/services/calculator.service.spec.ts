374b4b5ffc033d5533f0385678a75ab36bed4e59
Ali Hassan <engineralihassan@gmail.com>
2024-08-02 12:44:24 -0400

    structuring the test cases with beforEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,13 +2,16 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 describe('CalculatorService',()=>{
+    let calculator : CalculatorService;
+    let loggerSpy:any;
+    beforeEach(()=>{
+        loggerSpy = jasmine.createSpyObj('LoggerService',['log']);
+        calculator = new CalculatorService(loggerSpy);
+    })
     it('Should add two numbers',()=>{
-        const logger = new LoggerService();
-        spyOn(logger,'log');
-        const calculator= new CalculatorService(logger);
        let result= calculator.add(2,5);
         expect(result).toBe(7); 
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     })
     it('Should subtract two numbers',()=>{
         const calculator= new CalculatorService(new LoggerService());
